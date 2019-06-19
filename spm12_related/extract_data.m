95c5cc8a85006d0b934fff6613fae35ef854352f
ywwang-notes <51767154+ywwang-notes@users.noreply.github.com>
2019-06-19 16:52:18 -0700

    Add files via upload

---
+++ spm12_related/extract_data.m
@@ -0,0 +1,16 @@
+addpath 'C:\Users\ywwang\Documents\MATLAB\spm12';
+TR = .72;
+[filelist,flag] = spm_select();
+% data_pts = spm_get_data(filelist,[30 20 30]'); % for psy221e data
+data_pts = spm_get_data(filelist,[46 44 39]');
+data_pts = normc(data_pts);
+% [48 34 60] precuneus
+% [46 44 39] a dark hole
+% [20 80 40] lateral PFC
+% [40 90 40] mPFC
+% [20 34 50] IPC?
+figure
+hist(data_pts)
+figure
+fft_plot(data_pts', 1/TR);
+kstest(data_pts) % if 1, not normal
---
+++ spm12_related/fft_plot.m
@@ -0,0 +1,15 @@
+function fft_plot(X, Fs)
+% X = task(1:100);
+% X = s(3,11:110);
+L = length(X);
+
+Y = fft(X);
+P2 = abs(Y/L);
+P1 = P2(1:L/2+1);
+P1(2:end-1) = 2*P1(2:end-1);
+f = Fs*(0:(L/2))/L;
+plot(f,P1) 
+title('Single-Sided Amplitude Spectrum of X(t)')
+xlabel('f (Hz)')
+ylabel('|P1(f)|')
+end
