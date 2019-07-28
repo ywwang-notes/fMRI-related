b1c37cdeae48e6d1bafa222d8a0e4577a7ee2396
ywwang-notes <51767154+ywwang-notes@users.noreply.github.com>
2019-07-27 18:12:19 -0700

    Create group_simple.m

---
+++ spm12_related/group_simple.m
@@ -0,0 +1,26 @@
+% note: if you would like to create batch scripts for each subject, 
+% use group_batch.m instead.
+
+spm('defaults','fmri');
+spm_jobman('initcfg');
+
+upperdir = ''; % revise here
+
+% set up for jobs
+clear matlabbatch;
+
+sidlst = [];
+% I use 4 digit numbers as folder names
+
+for sbj=1:length(sidlst)
+    sid = num2str(sidlst(sbj), '%04i'); % revise here
+    run_n = sbj; % revise it according to your design
+    
+    % copy and paste template matlabbatch created by batch editor 
+    % replace the index with run_n, eg. matlabbatch{run_n}
+    
+end % end of loop thru subject folders
+
+spm_jobman('run',matlabbatch);
+
+% movefile(['.nii'], target_dir); % move the output nii files to the target directories
