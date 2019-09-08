1f88affd996b3d26fd27e5949736c0c0744b8a50
ywwang-notes <51767154+ywwang-notes@users.noreply.github.com>
2019-09-08 02:23:24 -0700

    Delete query_con.m

--- spm12_related/query_con.m
+++
@@ -1,9 +0,0 @@
-function[con_list] = query_con(ev, SPM)
-% Usage: please load SPM.mat before query
-% ev: (string) name of the event
-% example of adaptation: https://github.com/ywwang-notes/Personal-Projects/blob/master/spm12/query_con.m
-
-[row, col] = size(SPM.xCon);
-con_list = find(strcmp(target, {SPM.xCon(1:col).name}));
-
-end % function
